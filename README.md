f31828d1c90bd3366226b4828f631010181ed1ad
Taj <tajvirchahal87@gmail.com>
2025-06-14 12:35:14 -0400

    Update .gitignore, README, and fix to transcribe testing script; added requirements.txt

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 # Customer Satisfaction Analytics System
 
 This project aims to build a customer auditing analytics system that captures real-world conversations between staff and customers, analyzes the context and emotion behind those conversations, and presents insights through a simple dashboard. The goal is to help small and medium-sized businesses understand and improve customer experience by tying natural language feedback directly to business outcomes.
+
+
+
+#  Notes
+
+- Requires Installation of FFMPEG framework for OpenAI-Whisper to run locally. 
--- Scripts/transcribe.py
+++ Scripts/transcribe.py
@@ -4,14 +4,14 @@ import whisper, os
 import sys
 print(f"Using Python interpreter: {sys.executable}")
 
-file_path = "C:/Users/tajvi/OneDrive/Desktop/pythonProjects/NLP_Customer_Audit_Project/Data/call_center_set/call_recording_01.wav"
+file_path = "D:/Python Projects/NLP_Customer_Audit_Project/Data/archive/call_recording_02.wav"
 if os.path.exists(file_path):
     print("File exists!")
 else:
     print("File not found!")
 
 model = whisper.load_model("base")
-result = model.transcribe("call_recording_01.wav")
+result = model.transcribe("D:/Python Projects/NLP_Customer_Audit_Project/Data/archive/call_recording_02.wav")
 
 print(f' The text in video: \n {result["text"]}')
 
