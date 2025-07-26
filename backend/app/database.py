933b6fb76cb34c46ca7ba837db536720d4104db0
Taj <tajvirchahal87@gmail.com>
2025-07-26 13:59:01 -0400

    feat: update .gitignore to include *.sql and supabase; add database connection handling in database.py; create initial .vscode settings and extensions configuration

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,19 @@
 # backend/app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "postgresql://myuser:mypass@localhost:5432/customerdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
+#Format: postgresql://<username>:<password>@<host>:<port>/<database>
+#Supavisor Transaction Mode: postgres://postgres.apbkobhfnmcqqzqeeqss:[YOUR-PASSWORD]@aws-0-[REGION].pooler.supabase.com:6543/postgres
+
+
+
 
 engine = create_engine(DATABASE_URL)
+print("connection established successfully")
+
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
