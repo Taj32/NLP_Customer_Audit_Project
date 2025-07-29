26979ac8a672d1ebaf33f1de27091ad5dc34ea8b
Taj <tajvirchahal87@gmail.com>
2025-07-28 20:17:15 -0400

    chore: update CORS middleware to use environment variable for allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,12 +4,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import auth_routes
 from routes import convo_routes
+import os
 
 app = FastAPI()
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with your frontend's URL
+    allow_origins=ALLOWED_ORIGINS,  # Replace with your frontend's URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, OPTIONS, etc.)
     allow_headers=["*"],  # Allow all headers
